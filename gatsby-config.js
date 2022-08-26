d7c07729dad8519a202d760b9ec11e5c1f098118
Shidiq Ahmed <shidiq@pentafox.in>
2022-08-26 13:39:47 +0530

    refactor: pathPrefix added

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 
 
 module.exports = {
-  pathPrefix: null,
+  pathPrefix: "/website/",
   siteMetadata: {
     title: `pentafoxWeb`,
     siteUrl: `https://www.yourdomain.tld`,
