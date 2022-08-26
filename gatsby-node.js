30b5b40181e6788daf41e83e2b396904f6b14fc4
Shidiq Ahmed <shidiq@pentafox.in>
2022-08-26 11:52:32 +0530

    gatsby-ssr config added

---
+++ gatsby-node.js
@@ -0,0 +1,27 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+ const path = require('path');
+ const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
+ 
+ exports.onCreateWebpackConfig = ({
+   stage,
+   getConfig,
+   rules,
+   loaders,
+   actions,
+ }) => {
+   actions.setWebpackConfig({
+     resolve: {
+       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+       plugins: [new DirectoryNamedWebpackPlugin({
+         exclude: /node_modules/
+       })],
+     },
+   });
+ };
+ 
+ 
