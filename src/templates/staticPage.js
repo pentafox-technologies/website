c53ff00717c919442cb0c441a7af40ea245faf4f
Shidiq Ahmed <shidiq@pentafox.in>
2023-07-03 18:31:15 +0530

    intern terms and policies

---
+++ src/templates/staticPage.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import LayoutCommon from '../components/layout/layoutCommon'
+import { HomeWrapper } from '../components/home/home.css'
+import PageWrapper, { TopSectionWrapper } from '../components/pageWrapper/PageWrapper'
+import { Container, Title } from '@mantine/core'
+import { graphql, useStaticQuery } from 'gatsby'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+export const query = graphql`
+query ($slug: String!) {
+  contentfulStaticPage(slug: {eq: $slug}) {
+    title
+    slug
+    shortDescription {
+      shortDescription
+    }
+    content {
+      raw
+    }
+  }
+}`
+
+const StaticPage = (props) => {
+  const { content } = props.data.contentfulStaticPage;
+  const contentJson = JSON.parse(content.raw);
+  const richTextComponents = documentToReactComponents(contentJson)
+  
+  return (
+    <LayoutCommon hideLink showCareers={false} showDarkLogo={false} headProps={{ pageTitle: 'title'}} >
+      <HomeWrapper>
+        <PageWrapper>
+          <TopSectionWrapper>
+            <Container>
+              <Title>{props.data.contentfulStaticPage.title}</Title>
+            </Container>
+          </TopSectionWrapper>
+          <Container size='md' sx={{marginTop: 80, marginBottom: 80, overflowWrap: 'break-word'}}>
+            {richTextComponents}
+          </Container>
+        </PageWrapper>
+      </HomeWrapper>
+    </LayoutCommon>
+  )
+}
+
+export default StaticPage
